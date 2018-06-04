3042083c854194a1185edb4b24c36239063bdfea
Hugh A <harcenea@gmail.com>
2018-06-04 21:11:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  
+  katzDeliLine = [];
+  
+  katzDeliLine.push('ada');
+  
+  console.log(katzDeliLine);
+  
+  var position = katzDeliLine.length;
+  
+ return  `Welcome, ${name}. You are number ${position} in line.`;
+}
